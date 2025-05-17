6044a68bbd57017996381726722b2e32fc6b9335
sureshjoshi88 <sj3795789@gmail.com>
2025-05-17 14:04:18 +0530

    added new img

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import suresh from './images/suresh.jpg'
 
 const Home = () => {
   return (
     <div>
       <div className='background-image'>
-        <div>
-        <img src="" alt="" />
+        <div className='flex justify-center items-center h-200 me-5'>
+        <img className='h-70 w-70  rounded-4xl' src={suresh} alt="" />
         </div>
       </div>
     </div>
