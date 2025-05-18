aaa7b763459bf1b65a41508f73cb790e6b070bf3
sureshjoshi88 <sj3795789@gmail.com>
2025-05-18 12:28:03 +0530

    add new content and tag and updated code

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -6,11 +6,11 @@ const Home = () => {
     <>
     <div className='mt-5'>
       <div className='background-image'>
-        <div className='flex justify-around items-center h-150 '>
-          <div className='w-100'>
+        <div className='grid md:grid-cols-2 justify-around items-center h-150 '>
+          <div className='p-1 mt-3 flex justify-center'>
                     <img className='h-90 w-90   rounded-full' src={suresh} alt="" />
           </div>
-          <div className='w-170'>
+          <div className='p-1 mt-3'>
 
                     <p className='text-red-600 font-bold text-4xl text-shadow-white text-shadow-md'>My name is Suresh Joshi</p>
                     <p  className='text-green-500 font-bold text-3xl text-shadow-white text-shadow-md pt-2'> I am a Full Stack Developer</p>
@@ -23,8 +23,15 @@ const Home = () => {
       </div>
     </div>
 
+    <div className='grid grid-cols-3'>
     <div>
-
+      <h3 className='font-semibold text-2xl'>Tech Stack</h3>
+      <div >
+        <p>HTML | CSS | JavaScript | React | Node.js | Botstrap | Tailwind | Git | GitHub</p>
+      </div>
+    </div>
+    <div></div>
+    <div></div>
     </div>
     </>
   )
