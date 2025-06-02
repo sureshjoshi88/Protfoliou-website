e0d175b68737e9adcba29faf859a987034780252
sureshjoshi88 <sj3795789@gmail.com>
2025-06-02 14:14:59 +0530

    add new skilss

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -35,7 +35,7 @@ const Home = () => {
         <div className='p-2 mt-3'>
           <h3 className='font-semibold text-2xl'>Tech Stack</h3>
           <div >
-            <p>HTML | CSS | JavaScript | React | Botstrap | Tailwind | Git | GitHub</p>
+            <p>HTML | CSS | JavaScript | React | Botstrap | Tailwind | C++ | Git | GitHub</p>
           </div>
         </div>
         <div className='p-2 flex md:justify-center mt-3'>
